6312a6fefbf2851e8a599d328413f57942f5551f
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-06-19 09:39:14 +0300

    add save and search logic to task 2

--- 02_primitive_db/index.js
+++ 02_primitive_db/index.js
@@ -1,44 +1,136 @@
 import inquirer from 'inquirer';
+import * as fs from 'fs';
 
 let isRunning = true;
+const encoding = "utf8";
 
-while (isRunning) {
+function writeUserToDb(user) {
+    try {
+        fs.writeFileSync("db.txt", JSON.stringify(user).concat("\n"), {flag: 'a+', encoding: encoding});
+    } catch (err) {
+        console.log(err);
+        isRunning = false;
+    }
+}
+
+function readUsers() {
+    try {
+        const contents = fs.readFileSync("db.txt").toString(encoding);
+        return contents.split("\n").filter(x => x !== "").map(x => JSON.parse(x));
+    } catch (err) {
+        console.log(err);
+        isRunning = false;
+    }
+}
+
+function findUserByName(users, name) {
+    const normalizedName = name.toLocaleLowerCase();
+    return users.find((x) => x.username.toLocaleLowerCase() === normalizedName);
+}
+
+function handleInquirerPromptRejection() {
+    return (error) => {
+        if (error.isTtyError) {
+            console.error(error);
+            isRunning = false;
+        } else {
+            console.error(error);
+            isRunning = false;
+        }
+    };
+}
+
+function validateUsername() {
+    return async function (input) {
+        if (input.includes("\n"))
+            return "Newline character is not allowed in name!"; // because it is used as a separator in db
+        return true;
+    };
+}
+
+async function handleUserSearch() {
+    return inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "Do you wish to search for user? ",
+            }
+        ])
+        .then(async (answers) => {
+            if (answers.confirmation) {
+                const users = readUsers();
+                console.log(users);
+                const username = await inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "username",
+                        message: "Enter the name of the user, to terminate press ENTER: ",
+                        validate: validateUsername()
+                    }
+                ]).then(async (answers) => {
+                    return answers.username;
+                }).catch(handleInquirerPromptRejection());
+                const user = findUserByName(users, username);
+                if (user) {
+                    console.log(`User ${user.username} was found!`);
+                    console.log(user);
+                } else {
+                    console.log("No user by such name was found =(");
+                }
+            }
+        })
+        .catch(handleInquirerPromptRejection());
+}
+
+async function handleUserCreation() {
+    let username = "";
     await inquirer
         .prompt([
             {
                 type: "input",
                 name: "username",
-                message: "Enter the name of the user, to terminate press ENTER: "
-            },
-            {
-                type: "list",
-                name: "gender",
-                message: "Choose the user's gender: ",
-                choices: ["male", "female", "non-binary"]
-            },
-            {
-                type: "input",
-                name: "age",
-                message: "Enter user's age",
-                validate: async function (input) {
-                    const parsedInput = parseFloat(input);
-                    if (Number.isSafeInteger(parsedInput) && parsedInput > 0)
-                        return true;
-                    else
-                        return "Invalid age value!";
-                }
+                message: "Enter the name of the user, to terminate press ENTER: ",
+                validate: validateUsername()
             }
         ])
-        .then((answers) => {
-            console.log(answers);
-            if (answers.username === "")
+        .then(async (answers) => {
+            username = answers.username;
+            if (username === "") {
                 isRunning = false;
-        })
-        .catch((error) => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else went wrong
+                await handleUserSearch();
             }
-        });
+        })
+        .catch(handleInquirerPromptRejection());
+    if (isRunning)
+        await inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "gender",
+                    message: "Choose the user's gender: ",
+                    choices: ["male", "female", "non-binary"]
+                },
+                {
+                    type: "input",
+                    name: "age",
+                    message: "Enter user's age",
+                    validate: async function (input) {
+                        const parsedInput = parseFloat(input);
+                        if (Number.isSafeInteger(parsedInput) && parsedInput > 0)
+                            return true;
+                        else
+                            return "Invalid age value!";
+                    }
+                }
+            ])
+            .then(async (answers) => {
+                answers.username = username;
+                writeUserToDb(answers);
+            })
+            .catch(handleInquirerPromptRejection());
+}
+
+while (isRunning) {
+    await handleUserCreation();
 }
