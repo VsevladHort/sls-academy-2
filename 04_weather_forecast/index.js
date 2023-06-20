92bfd6c2adcd36b91606598d70252766e971d05d
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-06-20 12:24:05 +0300

    set up bot ui

--- 04_weather_forecast/index.js
+++ 04_weather_forecast/index.js
@@ -1,5 +1,56 @@
 import {readEnvVar} from "../03_telegram_console_sender/utils.js";
+import TelegramBot from "node-telegram-bot-api";
 
 readEnvVar("utf8");
 
-console.log(process.env.BOT_TOKEN);
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+
+bot.onText(/\/start/, async (msg) => {
+    try {
+        await bot.sendMessage(msg.chat.id, "Welcome, may i interest you in weather in Zaporizhzhia?", {
+            "reply_markup": {
+                "keyboard": [["Forecast in Zaporizhzhia"]]
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+bot.onText(/Forecast in Zaporizhzhia/, async (msg) => {
+    try {
+        await bot.sendMessage(msg.chat.id, "Great, at which intervals would you like to see the forecast?", {
+            "reply_markup": {
+                "keyboard": [["3 hour intervals"], ["6 hour intervals"]]
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+bot.onText(/3 hour intervals/, async (msg) => {
+    try {
+        await bot.sendMessage(msg.chat.id, "Wonderful, here is the weather forecast in 3-hour intervals!");
+        await bot.sendMessage(msg.chat.id, "Would you like to receive a new update on weather in Zaporizhzhia?", {
+            "reply_markup": {
+                "keyboard": [["Forecast in Zaporizhzhia"]]
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+bot.onText(/6 hour intervals/, async (msg) => {
+    try {
+        await bot.sendMessage(msg.chat.id, "Wonderful, here is the weather forecast in 6-hour intervals!");
+        await bot.sendMessage(msg.chat.id, "Would you like to receive a new update on weather in Zaporizhzhia?", {
+            "reply_markup": {
+                "keyboard": [["Forecast in Zaporizhzhia"]]
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+});
